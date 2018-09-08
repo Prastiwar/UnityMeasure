837fc03c1e992eb882e18091f20d02277829e48b
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-09-08 13:22:27 +0200

    Initial commit

---
+++ Source/SomeMono.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SomeMono : MonoBehaviour
+{
+    public int IntField;
+    public int IntProperty { get; set; }
+
+    private void OnValidate()
+    {
+
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
